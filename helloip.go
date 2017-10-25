35d3ba8c0f6e573f5d5add876195447f69a8a0a1
DonMills <dmills@singlestoneconsulting.com>
2017-10-25 16:15:41 -0400

    pushing v.04

--- helloip.go
+++ helloip.go
@@ -15,7 +15,7 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	ip, _ := eth.Addrs()
 	hostname, _ := os.Hostname()
 	fmt.Fprintf(w, "<h1>Hello World from %v!</h1>\n<b>My IP address is %v</b><br>\n", hostname, ip[0])
-	fmt.Fprintln(w, "This is Version v.03.")
+	fmt.Fprintln(w, "This is Version v.04.")
 }
 
 func main() {
